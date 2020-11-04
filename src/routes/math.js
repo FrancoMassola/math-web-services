c0887e0225c6340fb2ef2fa2209d507971ad5009
[FrancoMassola] <frann.massola@gmail.com>
2020-11-04 14:29:23 -0300

    fix functions

--- src/routes/math.js
+++ src/routes/math.js
@@ -6,28 +6,28 @@ router.get('/:expression/:presicion', (req,res)=>{
     //required parameters
     const string_expression = req.params.expression;
     const string_presicion = req.params.presicion;
-    //square root validation on string
+    //string square validation
     if (string_expression.indexOf('s')!=-1) {
         //function call to solve square root on math expression
-        var result = square_root_function(string_expression,string_presicion);
-        //function call to get significant numbers
-        var significantNumbers = get_significantDigits(result, string_presicion);
+        var math_resultSr = square_root_function(string_expression);
+        //function call to get significant digits
+        var significant_digits = get_significantDigits(math_resultSr, string_presicion);
         //send response to the client
-       res.send("El resultao es " + result + " --- Cifras significativas: " + significantNumbers);
+       res.send("El resultao es " + math_resultSr + " --- Cifras significativas: " + significant_digits);
       
       }
       else{
           //function call to solve math expression
-          var resultado_final = final(string_expression);
-          //creating a string object
-          var cadena_resultado = (new String(resultado_final)).valueOf();
-          //var initialization to store significant nombers of the math expression
-          var cantidad_digitos = '';
-          console.log("Resultado final: " + resultado_final);
-          //function call to get significant numbers
-          cantidad_digitos = get_significantDigits(cadena_resultado,string_presicion);
+          var math_result = math_expression_result(string_expression);
+          //create a string object
+          var math_stringR = (new String(math_result)).valueOf();
+          //var initialization to store significant digits of the math expression
+          var significantDigits = '';
+          console.log("Resultado final: " + math_result);
+          //function call to get significant digits
+          significantDigits = get_significantDigits(math_stringR,string_presicion);
           //send response to the client
-          res.send('Resultado de la expresion ' + resultado_final + ' --- Cifras significativas: ' + cantidad_digitos);
+          res.send('Resultado de la expresion ' + math_result + ' --- Cifras significativas: ' + significantDigits);
       }
 
 });
@@ -39,138 +39,139 @@ router.post('/', (req,res)=>{
     console.log(req.body);
     //require values from the request body
     const {expression,presicion} = req.body;
-    var cadena = (new String(expression)).valueOf();
-    var cifra_cignificativa = (new String(presicion)).valueOf();
-    //square root validation on string
-    if (cadena.indexOf('s')!=-1) {
-      var result = square_root_function(cadena,cifra_cignificativa);
-      var cifras_significativas_raiz = get_significantDigits(result, cifra_cignificativa);
-     res.send("El resultao es " + result + " --- Cifras significativas: " + cifras_significativas_raiz);
+    var stringExpression = (new String(expression)).valueOf();
+    var sting_significantDigits = (new String(presicion)).valueOf();
+    //string square validation
+    if (stringExpression.indexOf('s')!=-1) {
+      var mathResult = square_root_function(stringExpression);
+      var significantDigits = get_significantDigits(mathResult, sting_significantDigits);
+     res.send("El resultao es " + mathResult + " --- Cifras significativas: " + significantDigits);
     
     }
     else{
-        //if the expression not contain square root ejecute function call to get a expression result
-        var resultado_final = final(cadena);
-        var cadena_resultado = (new String(resultado_final)).valueOf();
-        var cantidad_digitos = '';
-        console.log("Resultado final: " + resultado_final);
-        //function call to get significant numbers
-        cantidad_digitos = get_significantDigits(cadena_resultado,cifra_cignificativa);
+        //if the expression not contain square root ejecute math_expression_result function to get a expression result
+        var finalResult = math_expression_result(stringExpression);
+        var stringMath_result = (new String(finalResult)).valueOf();
+        var digits_notRoot_result = '';
+        console.log("Resultado final: " + finalResult);
+        //function call to get significant digits
+        digits_notRoot_result = get_significantDigits(stringMath_result,sting_significantDigits);
 
-        res.send('Resultado de la expresion ' + resultado_final + ' --- Cifras significativas: ' + cantidad_digitos);
+        res.send('Resultado de la expresion ' + finalResult + ' --- Cifras significativas: ' + digits_notRoot_result);
     }
     
 
 });
 
 //function to solve a square root of the mathematical expression
-function square_root_function(cadena){
+function square_root_function(valueStringExpression){
     //constants to get the position of the square root
-    const i_position = cadena.indexOf('t');
-    const end_position = cadena.indexOf(')');
+    const i_position = valueStringExpression.indexOf('t');
+    const end_position = valueStringExpression.indexOf(')');
     console.log(i_position);
     console.log(end_position);
     //get string inside square root expression
-    var sub = cadena.substring(i_position+2,end_position);
-    console.log(sub);
+    var stringSquareRoot = valueStringExpression.substring(i_position+2,end_position);
+    console.log(stringSquareRoot);
     console.log("existe raiz");
     console.log("existe raiz");
-    console.log("Valor: " + sub);
+    console.log("Valor: " + stringSquareRoot);
     //solve the square root
-    const resultado = eval("Math.sqrt("+sub+")");
-    console.log(resultado);
+    const result_square_root = eval("Math.sqrt("+stringSquareRoot+")");
+    console.log(result_square_root);
     //get the mathematical expression that is before the square root
-    const expresion_ant = cadena.substring(0,cadena.indexOf('s'));
-    const sub_expresion_post = cadena.slice(cadena.indexOf('s'));
+    const beforeExpression = valueStringExpression.substring(0,valueStringExpression.indexOf('s'));
+    const sub_afterExpression = valueStringExpression.slice(valueStringExpression.indexOf('s'));
     //get the mathematical expression that is after the square root
-    const expresion_post = sub_expresion_post.slice(sub_expresion_post.indexOf(')')+1);
+    const afterExpression = sub_afterExpression.slice(sub_afterExpression.indexOf(')')+1);
 
-    console.log("expresion anterior" + expresion_ant);
-    console.log("expresion posterior" + expresion_post);
+    console.log("expresion anterior" + beforeExpression);
+    console.log("expresion posterior" + afterExpression);
     //concatenate the entire mathematical expression
-    const valor_concatenado = (expresion_ant+resultado+expresion_post);
+    const stringExpressionComplete = (beforeExpression+result_square_root+afterExpression);
     //solve the entire mathematical expression
-    var valor_cadena_final = eval(valor_concatenado);
+    var result_math_complete = eval(stringExpressionComplete);
   
-     console.log("EL RESULTADO FINAL CON RAIZ ES: " + valor_cadena_final);
+     console.log("EL RESULTADO FINAL CON RAIZ ES: " + result_math_complete);
 
-    return valor_cadena_final;
+    return result_math_complete;
 }
 
 
 
 //function to solve the entire mathematical expression
-function final(valor_cadena) {
+function math_expression_result(stringExpressionValue) {
 
-   var valor_cadena_final = eval(valor_cadena);
+   var finalExpressionValue = eval(stringExpressionValue);
 
-   return valor_cadena_final;
+   return finalExpressionValue;
     
 }
 
 //function to get the significant digits of the result
-function get_significantDigits (valor_cadena_final,cifra_cignificativa) {
+function get_significantDigits (string_finalValue,significativeDigits) {
 
-     var cadena_valor = (new String(valor_cadena_final)).valueOf();
+     var string_resultValue = (new String(string_finalValue)).valueOf();
      var result = "";
 
      //conditional to evaluate decimal digits
-     if (cadena_valor.includes('0.')){
+     if (string_resultValue.includes('0.')){
      //get an array digits of the result 
-     for (i=0;i<cadena_valor.length-1;i++) { 
-        result += cadena_valor.charAt(i) 
+     for (i=0;i<string_resultValue.length-1;i++) { 
+        result += string_resultValue.charAt(i) 
         result += "-" 
  } 
     console.log(result);
-    var array_digito = result.split("-");
+    var digistArray = result.split("-");
 
-    console.log(array_digito[1]);
-     var posicion_digito = 0;
+    console.log(digistArray[1]);
+     var digitPosition = 0;
      var i = 0;   
 
     //get the position of the significant digits after the 0.
-     while(array_digito[i]=='0'||array_digito[i]=='.' ||array_digito[i] == '-0'){
-         posicion_digito = i;
+     while(digistArray[i]=='0'||digistArray[i]=='.' ||digistArray[i] == '-0'){
+         digitPosition = i;
          i++;
      }
-     console.log("Posicion digito" + posicion_digito);
-     console.log("recorrer digito" + cifra_cignificativa);
+     console.log("Posicion digito" + digitPosition);
+     console.log("recorrer digito" + significativeDigits);
      
-     var resultado2 = posicion_digito + eval(cifra_cignificativa+"+1")-1;
-     console.log(resultado2);
+     var totalDigitResult = digitPosition + eval(significativeDigits+"+1")-1;
+     console.log(totalDigitResult);
 
-     var array1 = [];
+     var finalArray = [];
      //get array of the significat digits
-     for(var j = posicion_digito+1;j<=resultado2;j++){
+     for(var j = digitPosition+1;j<=totalDigitResult;j++){
 
-        array1 = array1.concat(array_digito[j]);
+        finalArray = finalArray.concat(digistArray[j]);
 
      }
     //return the array as a string
-     return array1.join('');
+     return finalArray.join('');
    
     }
     else{
-       var string_value = 0;
-       string_value = parseFloat(cadena_valor);
-       console.log(string_value);
-        if(string_value>=1&&Number.isInteger(string_value)){
-        var digito1 = cadena_valor.substring(0,eval(cifra_cignificativa));
-        console.log("Valores significativos: " + digito1);
+       var resultValue = 0;
+       //convert the result value to float to evaluate the conditionals 
+       resultValue = parseFloat(string_resultValue);
+       console.log(resultValue);
+        if(resultValue>=1&&Number.isInteger(resultValue)){
+        var sDigits = string_resultValue.substring(0,eval(significativeDigits));
+        console.log("Valores significativos: " + sDigits);
 
-        return digito1;
+        return sDigits;
         }
-        else if(!Number.isInteger(string_value)&&string_value>1000) {
-            var digito = cadena_valor.substring(0,eval(cifra_cignificativa));
-        console.log("Valores significativossss: " + digito);
+        else if(!Number.isInteger(resultValue)&&resultValue>1000) {
+            var sDigits2 = string_resultValue.substring(0,eval(significativeDigits));
+        console.log("Valores significativoss: " + sDigits2);
 
-        return digito;
+        return sDigits2;
         }
         else {
-            var digito = cadena_valor.substring(0,eval(cifra_cignificativa+"+1"));
-        console.log("Valores significativossss222: " + digito);
+            var sDigits3 = string_resultValue.substring(0,eval(significativeDigits+"+1"));
+        console.log("Valores significativossss222: " + sDigits3);
 
-        return digito;
+        return sDigits3;
         }
     }
      

e223f363cb5de68fd5c3f73f26ae2a0960d3fd06
[FrancoMassola] <frann.massola@gmail.com>
2020-11-04 16:06:23 -0300

    add app.yaml

--- src/routes/math.js
+++ src/routes/math.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const router = Router();
-const myModule = require('../math_functions/math_functions');
+const mathFunctionsMolude = require('../math_functions/math_functions');
+
 //router GET 
 router.get('/:expression/:presicion', (req,res)=>{
     //required parameters
@@ -9,22 +10,22 @@ router.get('/:expression/:presicion', (req,res)=>{
     //string square validation
     if (string_expression.indexOf('s')!=-1) {
         //function call to solve square root on math expression
-        var math_resultSr = myModule.square_root_function(string_expression);
+        var math_resultSr = mathFunctionsMolude.square_root_function(string_expression);
         //function call to get significant digits
-        var significant_digits = myModule.get_significantDigits(math_resultSr, string_presicion);
+        var significant_digits = mathFunctionsMolude.get_significantDigits(math_resultSr, string_presicion);
         //send response to the client
        res.send("The result of math expression is: " + math_resultSr + " --- The significant digits are: " + significant_digits);
       
       }
       else{
           //function call to solve math expression
-          var math_result = myModule.math_expression_result(string_expression);
+          var math_result = mathFunctionsMolude.math_expression_result(string_expression);
           //create a string object
           var math_stringR = (new String(math_result)).valueOf();
           //var initialization to store significant digits of the math expression
           var significantDigits = '';
           //function call to get significant digits
-          significantDigits = myModule.get_significantDigits(math_stringR,string_presicion);
+          significantDigits = mathFunctionsMolude.get_significantDigits(math_stringR,string_presicion);
           //send response to the client
           res.send("The result of math expression is: " + math_result + " --- The significant digits are: " + significantDigits);
       }
@@ -42,19 +43,19 @@ router.post('/', (req,res)=>{
     var sting_significantDigits = (new String(presicion)).valueOf();
     //string square validation
     if (stringExpression.indexOf('s')!=-1) {
-      var mathResult = myModule.square_root_function(stringExpression);
-      var significantDigits = myModule.get_significantDigits(mathResult, sting_significantDigits);
+      var mathResult = mathFunctionsMolude.square_root_function(stringExpression);
+      var significantDigits = mathFunctionsMolude.get_significantDigits(mathResult, sting_significantDigits);
      res.send("The result of math expression is: "  + mathResult + " --- The significant digits are: " + significantDigits);
     
     }
     else{
         //if the expression does not contain square root, ejecute math_expression_result function to get a expression result
-        var finalResult = myModule.math_expression_result(stringExpression);
+        var finalResult = mathFunctionsMolude.math_expression_result(stringExpression);
         var stringMath_result = (new String(finalResult)).valueOf();
         var digits_notRoot_result = '';
         console.log("Resultado final: " + finalResult);
         //function call to get significant digits
-        digits_notRoot_result = myModule.get_significantDigits(stringMath_result,sting_significantDigits);
+        digits_notRoot_result = mathFunctionsMolude.get_significantDigits(stringMath_result,sting_significantDigits);
         res.send("The result of math expression is: " + finalResult + " --- The significant digits are: " + digits_notRoot_result);
     }
     
